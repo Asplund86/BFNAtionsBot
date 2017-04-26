9dd8dbb61bcc419ecdf5f2376e54b4d15b9d6df8
Emil Asplund <emilasplund86@gmail.com>
2017-04-26 19:11:41 +0200

    Adding files

---
+++ index.js
@@ -0,0 +1,8 @@
+//Load Discord
+const Discord = require('discord.js');
+//Creating bot
+const bot = new Discord.Client();
+
+//Bot login
+bot.login('MzA2ODMyMzk2MjkxMDE0NjU2.C-Jfew.WJJfF2aD6dnp4dE7s8gf_GXXS50');
+console.log('BFNations online!');
